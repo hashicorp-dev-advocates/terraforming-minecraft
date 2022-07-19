822b12c91b1effdbb61243bd1be13513d90295a2
Erik Veld <eveld@hashicorp.com>
2022-07-19 09:44:00 +0200

    Add infra

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terraforming Minecraft
+
+This is an example of how to write Terraform code and use a Git workflow to manage resources.
+
+In this example we use the Minecraft Terraform provider to give a visual representation of what Terraform does in the background.
---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    minecraft = {
+      source  = "local/hashicraft/minecraft"
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "minecraft" {
+  address  = "localhost:27015"
+  password = "password"
+}
+
+resource "minecraft_block" "stone" {
+  material = "minecraft:stone"
+
+  position = {
+    x = -198
+    y = 66
+    z = -195
+  }
+}
