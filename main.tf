0504a0512e15293b2d6a2508048b9b11265e783f
Erik Veld <eveld@hashicorp.com>
2022-07-19 14:53:30 +0200

    Clean up

--- main.tf
+++ main.tf
@@ -12,73 +12,71 @@ provider "minecraft" {
   password = "password"
 }
 
-
-
 // Module that creates a cube out of Minecraft blocks
-module "walls" {
-  source = "./cube"
+# module "walls" {
+#   source = "./cube"
 
-  material = "stone"
+#   material = "stone"
 
-  position = {
-    x = -198,
-    y = 68,
-    z = -195
-  }
+#   position = {
+#     x = -198,
+#     y = 68,
+#     z = -195
+#   }
 
-  dimensions = {
-    width  = 9,
-    length = 9,
-    height = 4
-  }
-}
+#   dimensions = {
+#     width  = 9,
+#     length = 9,
+#     height = 4
+#   }
+# }
 
-module "inside" {
-  depends_on = [
-    module.walls
-  ]
+# module "inside" {
+#   depends_on = [
+#     module.walls
+#   ]
 
-  source = "./cube"
+#   source = "./cube"
 
-  material = "air"
+#   material = "air"
 
-  position = {
-    x = -197,
-    y = 69,
-    z = -194
-  }
+#   position = {
+#     x = -197,
+#     y = 69,
+#     z = -194
+#   }
 
-  dimensions = {
-    width  = 7,
-    length = 7,
-    height = 3
-  }
-}
+#   dimensions = {
+#     width  = 7,
+#     length = 7,
+#     height = 3
+#   }
+# }
 
-resource "minecraft_block" "furnace" {
-  depends_on = [
-    module.inside
-  ]
+# resource "minecraft_block" "furnace" {
+#   depends_on = [
+#     module.inside
+#   ]
 
-  material = "minecraft:furnace"
+#   material = "minecraft:furnace"
 
-  position = {
-    x = -196
-    y = 69
-    z = -188
-  }
-}
+#   position = {
+#     x = -196
+#     y = 69
+#     z = -188
+#   }
+# }
 
-resource "minecraft_block" "chest" {
-  depends_on = [
-    module.inside
-  ]
+# resource "minecraft_block" "chest" {
+#   depends_on = [
+#     module.inside
+#   ]
 
-  material = "minecraft:chest"
+#   material = "minecraft:chest"
 
-  position = {
-    x = -195
-    y = 69
-    z = -188
-  }
-}
+#   position = {
+#     x = -195
+#     y = 69
+#     z = -188
+#   }
+# }
